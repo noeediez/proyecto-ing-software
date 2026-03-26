4939ec17743fc5924be41d767eefdd1ec2485d6f
noeediez <noediezxd@gmail.com>
2026-03-26 02:26:27 +0000

    base para la plataforma (basico)

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log("hola, funciona typescript");
+class Cancion {
+    constructor(// 
+    titulo, // propiedades privadas
+    artista, duracion) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracion = duracion;
+    }
+    mostrarInfo() {
+        return `Titulo: ${this.titulo} | Artista: ${this.artista} | Duracion: ${this.duracion} minutos`;
+    }
+    reproducir() {
+        return `Reproduciendo: ${this.titulo} de ${this.artista}`;
+    }
+}
+class PlataformadeMusica {
+    constructor() {
+        this.canciones = []; // propiedad privada
+    }
+    agregarCancion(cancion) {
+        this.canciones.push(cancion);
+    }
+    mostrarCanciones() {
+        return this.canciones.map(c => c.mostrarInfo()).join("\n");
+    }
+}
+const cancion1 = new Cancion("The Subway", "Chappell Roan", 4.1); // instancia de la clase Cancion
+const cancion2 = new Cancion("What Was That", "Lorde", 4.3); // instancia de la clase Cancion
+const plataforma = new PlataformadeMusica(); // instancia de la clase PlataformadeMusica
+plataforma.agregarCancion(cancion1);
+plataforma.agregarCancion(cancion2);
+console.log(plataforma.mostrarCanciones());
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
-console.log("hola, funciona typescript");
+class Cancion { // clase 
+    constructor ( // 
+        private titulo: string, // propiedades privadas
+        private artista: string,
+        private duracion: number
+    ) {}
+
+    public mostrarInfo(): string {  //metodo publico 
+        return `Titulo: ${this.titulo} | Artista: ${this.artista} | Duracion: ${this.duracion} minutos`;
+    }
+
+    public reproducir(): string { //metodo publico x2
+        return `Reproduciendo: ${this.titulo} de ${this.artista}`;
+    }
+}
+ 
+class PlataformadeMusica { // clase
+    private canciones: Cancion[] = []; // propiedad privada
+
+    public agregarCancion(cancion: Cancion): void { // metodo publico
+        this.canciones.push(cancion);
+    }
+    
+    public mostrarCanciones(): string { // metodo publico x2
+        return this.canciones.map(c => c.mostrarInfo()).join("\n");
+    }
+}
+
+const cancion1 = new Cancion("The Subway", "Chappell Roan", 4.1); // instancia de la clase Cancion
+const cancion2 = new Cancion("What Was That", "Lorde", 4.3); // instancia de la clase Cancion
+
+const plataforma = new PlataformadeMusica(); // instancia de la clase PlataformadeMusica
+
+plataforma.agregarCancion(cancion1);
+plataforma.agregarCancion(cancion2);
+
+console.log(plataforma.mostrarCanciones()); //console log sirve para mostrar la informacion de las canciones 
